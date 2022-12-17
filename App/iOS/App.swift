2a460f849219587278bc43a36fc03f1f80f0fb11
Maciej Górecki <maciekgorecki@icloud.com>
2022-12-17 12:12:41 +0100

    project setup

---
+++ App/iOS/App.swift
@@ -0,0 +1,12 @@
+//  Created by Maciej Gorecki on 17/12/2022.
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
